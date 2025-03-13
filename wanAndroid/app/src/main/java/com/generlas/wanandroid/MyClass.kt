1b92eca3e8dec37ded286a9c906487d127e509e9
generals <1487144524@qq.com>
2025-03-13 16:33:28 +0800

    Update TaskList

---
+++ wanAndroid/app/src/main/java/com/generlas/wanandroid/MyClass.kt
@@ -0,0 +1,8 @@
+package com.generlas.wanandroid
+
+/**
+ * description ： TODO:类的作用
+ * date : 2025/3/12 20:08
+ */
+class MyClass {
+}
---
+++ wanAndroid/app/src/main/java/com/generlas/wanandroid/ReflectStudy.kt
@@ -0,0 +1,11 @@
+package com.generlas.wanandroid
+
+/**
+ * description ： TODO:类的作用
+ * date : 2025/3/12 19:33
+ */
+
+fun main() {
+    val classObj : Class<MyClass> = MyClass::class.java //Class.forName(className)
+
+}
